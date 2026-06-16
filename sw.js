940aa4b058776a53bf5292be56a76a5694d8921d
davidjaquet-lgtm <davidjaquet@live.fr>
2026-06-16 10:15:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ImmoConnect — Service Worker v1.0
 // Cache offline + notifications push
 
-const CACHE_NAME = 'immoconnect-v11';
-const CACHE_STATIC = 'immoconnect-static-v11';
+const CACHE_NAME = 'immoconnect-v12';
+const CACHE_STATIC = 'immoconnect-static-v12';
 
 // Pages à mettre en cache pour fonctionnement offline
 const STATIC_ASSETS = [
